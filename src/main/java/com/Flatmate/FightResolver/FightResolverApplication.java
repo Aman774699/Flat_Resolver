f3058d60340ea36e3073c5ec5e8cd9681b649150
Aman774699 <amanvishwakarma922@gmail.com>
2025-02-19 14:39:10 +0530

    Initial commit

---
+++ src/main/java/com/Flatmate/FightResolver/FightResolverApplication.java
@@ -0,0 +1,13 @@
+package com.Flatmate.FightResolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Flatmate/FightResolver/FightResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Flatmate.FightResolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
